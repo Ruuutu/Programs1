ace7649dd1863a71b7eacf11639ba9150777bb12
Rutuja Arjun Wakade <134030978+Ruuutu@users.noreply.github.com>
2024-07-23 17:07:36 +0530

    Add files via upload

---
+++ PROGRAMS1/AllChar3.java
@@ -0,0 +1,45 @@
+//all character in java using for
+/*class P3
+{
+    public static void main(String args[])
+    {
+        for(int count=0;count<=127;count++)
+        {
+            System.out.println(count + "="+(char)count);
+
+        }
+    }
+
+}
+*/
+//all character in java using while
+/*class P3
+{
+    public static void main(String args[])
+    {
+        int count=0;
+        while(count<=127)
+        {
+            System.out.println(count + "  "+(char)count);
+            count++;
+
+        }
+    }
+}
+*/
+//all character in java using do-while
+class AllChar3
+{
+    public static void main(String args[])
+    {
+        int count=0;
+        do
+        {
+            System.out.println(count + "    "+(char)count);
+            count++;
+        }
+            while(count<=127);
+    }
+ }
+
+
